97668eec473a3e5dc16de11d9560c4f2a967276a
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2020-03-19 08:28:58 -0300

    Restore App.js

--- electron/App.js
+++ electron/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+  import { dark as darkTheme, light as lightTheme, mapping } from '@eva-design/eva';
+
+  
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+  </View>
   );
 }
 
